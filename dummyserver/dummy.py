566665672af73530f3bb0bd0dcbce2cf23e8b086
Yuan-T-Xuan <tonyxuan26@foxmail.com>
2017-11-17 15:38:10 -0500

    .

--- dummyserver/dummy.py
+++ dummyserver/dummy.py
@@ -7,7 +7,7 @@ import datetime
 app = Flask(__name__)
 CORS(app)
 
-pathTo = '/Users/ericnguyen/BoData-Project/dummyserver/'
+pathTo = '/Users/xuan/Desktop/BoData-Project/dummyserver/'
 
 @app.route('/statics/')
 @app.route('/statics/<name>')
